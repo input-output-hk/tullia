d1c0c6ec50795fd4c3c1036fe73b767b302a1be8
Michael Fellinger <michael.fellinger@iohk.io>
2022-05-03 13:51:03 +0200

    better version of bump

--- bump.rb
+++ bump.rb
@@ -1,14 +1,23 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'json'
 require 'date'
 require 'open3'
+require 'English'
 
 pkg = '.#defaultPackage.x86_64-linux'
-attrs = `nix eval --json "#{pkg}" --apply \
-'p: { inherit (builtins.unsafeGetAttrPos "pname" p) file; inherit (p) vendorSha256 version; }'`
-file, old_sha, version = JSON.parse(attrs).values_at('file', 'vendorSha256', 'version')
-file = File.join(file.split('/')[4..-1])
+attrs = `nix eval --json "#{pkg}" --apply 'p: { inherit (p) vendorSha256 version; }'`
+raise "couldn't get package info" unless $CHILD_STATUS.success?
+
+old_sha, version = JSON.parse(attrs).values_at('vendorSha256', 'version')
+
+needle = /#{Regexp.escape(old_sha)}/
+file = Dir.glob('**/*.nix').find { |path| File.open(path) { |fd| fd.grep(needle).any? } }
+
+raise "couldn't find file containing #{old_sha}" unless file
+
+puts "#{file} is at #{old_sha} #{version}"
 
 puts 'Checking vendorSha256...'
 
@@ -16,7 +25,7 @@ new_sha = nil
 
 Open3.popen3('nix', 'build', "#{pkg}.invalidHash") do |_si, _so, se|
   se.each_line do |line|
-    new_sha = $~[:sha] if line =~ /^\s+got:\s+(?<sha>sha256-\S+)$/
+    new_sha = $LAST_MATCH_INFO[:sha] if line =~ /^\s+got:\s+(?<sha>sha256-\S+)$/
   end
 end
 
@@ -25,7 +34,8 @@ if old_sha == new_sha
   exit
 else
   puts "Updating vendorSha256 #{old_sha} => #{new_sha}"
-  updated = File.read(file).gsub(old_sha, new_sha)
+  updated = File.read(file).gsub(/#{Regexp.escape(old_sha)}/, new_sha)
+  puts updated
   File.write(file, updated)
 end
 
