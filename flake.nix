6f0f55f46581b10552d1304ab27b0b1c72754622
David Arnold <david.arnold@iohk.io>
2022-05-13 21:59:35 -0500

    stdize a little more and expose `tasks`-clade

--- flake.nix
+++ flake.nix
@@ -8,26 +8,33 @@
   };
 
   outputs = inputs: let
-    mkTullia = import ./nix/std.nix inputs;
+    tasks = import ./nix/std.nix inputs;
+    lib = import ./nix/lib.nix inputs;
   in
-    (inputs.std.growOn {
-        inherit inputs;
-        cellsFrom = ./cells;
-        organelles = [
-          (inputs.std.functions "library")
-          (mkTullia "task")
-          (inputs.std.functions "action")
-          (inputs.std.devshells "devshell")
-          (inputs.std.installables "apps")
-        ];
-      }
-      {
-        devShell = inputs.std.harvest inputs.self ["tullia" "devshell" "default"];
-        defaultPackage = inputs.std.harvest inputs.self ["tullia" "apps" "tullia"];
-      })
-    // ((import ./nix/lib.nix inputs).fromStd {
-        actions = inputs.std.harvest inputs.self ["tullia" "action"];
-        tasks = inputs.std.harvest inputs.self ["tullia" "task"];
-      }
-      // (import ./nix/lib.nix inputs));
+    inputs.std.growOn {
+      inherit inputs;
+      cellsFrom = ./cells;
+      organelles = [
+        (inputs.std.functions "library")
+        (inputs.std.devshells "devshell")
+        (inputs.std.installables "apps")
+        # dog food
+        (tasks "task")
+        (inputs.std.functions "action")
+      ];
+    }
+    # Soil ...
+    # nix-cli compat
+    {
+      devShell = inputs.std.harvest inputs.self ["tullia" "devshell" "default"];
+      defaultPackage = inputs.std.harvest inputs.self ["tullia" "apps" "tullia"];
+    }
+    # dog food
+    (lib.fromStd {
+      actions = inputs.std.harvest inputs.self ["tullia" "action"];
+      tasks = inputs.std.harvest inputs.self ["tullia" "task"];
+    })
+    # top level tullia outputs
+    (lib // {inherit tasks;})
+    ;
 }
