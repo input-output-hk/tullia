ac52c4e3e338f9b4cb85436dac5e4d9f77211ef6
Robin Stumm <robin.stumm@iohk.io>
2022-07-11 14:39:49 +0200

    treefmt

--- nix/preset/nix.nix
+++ nix/preset/nix.nix
@@ -15,30 +15,31 @@
         "https://hydra.iohk.io" = "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
         "https://cache.ci.iog.io" = "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
       };
-    in lib.mkBefore [
-      (
-        pkgs.runCommand "etc" {} ''
-          mkdir -p $out/etc
-          cd $out/etc
+    in
+      lib.mkBefore [
+        (
+          pkgs.runCommand "etc" {} ''
+            mkdir -p $out/etc
+            cd $out/etc
 
-          echo > passwd 'nixbld1:x:1000:100:Nix build user 1:/local:/bin/sh'
-          echo > shadow 'nixbld1:!:1::::::'
-          echo > group  'nixbld:x:100:nixbld1'
-          echo > subgid 'nixbld1:1000:100'
-          echo > subuid 'nixbld1:1000:100'
+            echo > passwd 'nixbld1:x:1000:100:Nix build user 1:/local:/bin/sh'
+            echo > shadow 'nixbld1:!:1::::::'
+            echo > group  'nixbld:x:100:nixbld1'
+            echo > subgid 'nixbld1:1000:100'
+            echo > subuid 'nixbld1:1000:100'
 
-          mkdir nix
-          cat <<EOF > nix/nix.conf
-          experimental-features = ca-derivations flakes nix-command recursive-nix
-          log-lines = 1000
-          show-trace = true
-          sandbox = false
-          substituters = ${toString (__attrNames substituters)}
-          trusted-public-keys = ${toString (__attrValues substituters)}
-          EOF
-        ''
-      )
-    ];
+            mkdir nix
+            cat <<EOF > nix/nix.conf
+            experimental-features = ca-derivations flakes nix-command recursive-nix
+            log-lines = 1000
+            show-trace = true
+            sandbox = false
+            substituters = ${toString (__attrNames substituters)}
+            trusted-public-keys = ${toString (__attrValues substituters)}
+            EOF
+          ''
+        )
+      ];
 
     dependencies = with pkgs; [coreutils gitMinimal nix];
 
