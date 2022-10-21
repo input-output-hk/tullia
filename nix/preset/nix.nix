2c9b995942de7d341330af105f3ef13c15e33cb3
GTrunSec <gtrunsec@hardenedlinux.org>
2022-10-21 09:32:45 -0700

    add NIX_CONF_DIR in preset.nix.env

--- nix/preset/nix.nix
+++ nix/preset/nix.nix
@@ -3,38 +3,39 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  nixConf = let
+    substituters = {
+      "https://cache.nixos.org" = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
+      "https://hydra.iohk.io" = "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
+      "https://cache.ci.iog.io" = "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
+    };
+  in
+    pkgs.runCommand "etc" {} ''
+      mkdir -p $out/etc/nix
+      cat <<EOF > $out/etc/nix/nix.conf
+      experimental-features = ca-derivations flakes nix-command recursive-nix
+      log-lines = 1000
+      show-trace = true
+      sandbox = false
+      substituters = ${toString (__attrNames substituters)}
+      trusted-public-keys = ${toString (__attrValues substituters)}
+      EOF
+    '';
+in {
   options.preset.nix.enable = lib.mkEnableOption "nix preset";
 
   config = lib.mkIf config.preset.nix.enable {
     nsjail.mount."/tmp".options.size = 1024;
     nsjail.bindmount.ro = lib.mkBefore ["${config.closure.closure}/registration:/registration"];
-    oci.contents = let
-      substituters = {
-        "https://cache.nixos.org" = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
-        "https://hydra.iohk.io" = "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
-        "https://cache.ci.iog.io" = "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
-      };
-    in
-      lib.mkBefore [
-        (
-          pkgs.runCommand "etc" {} ''
-            mkdir -p $out/etc/nix
-            cat <<EOF > $out/etc/nix/nix.conf
-            experimental-features = ca-derivations flakes nix-command recursive-nix
-            log-lines = 1000
-            show-trace = true
-            sandbox = false
-            substituters = ${toString (__attrNames substituters)}
-            trusted-public-keys = ${toString (__attrValues substituters)}
-            EOF
-          ''
-        )
-      ];
+    oci.contents = lib.mkBefore [nixConf];
 
     dependencies = with pkgs; [coreutils gitMinimal nix];
 
-    env.USER = lib.mkDefault "nixbld1";
+    env = {
+      USER = lib.mkDefault "nixbld1";
+      NIX_CONF_DIR = "${nixConf}/etc/nix";
+    };
 
     commands = lib.mkOrder 300 [
       {
