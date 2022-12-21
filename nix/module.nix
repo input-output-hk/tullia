input-output-hk/tullia

commit 1cb7cffbba1ce1a7146d1e310b167f2309db08d5

create /repo in nomad job

nix/module.nix
@@ -1344,6 +1344,13 @@ in {
             dependencies = [pkgs.tullia];
 
             command.text = ''
+              set +x
+              if [[ -n "$NOMAD_JOB_ID" ]]; then
+                mkdir /repo
+                cd /repo
+              fi
+              set -x
+
               exec tullia run ${lib.escapeShellArg name}
             '';
 
@@ -1386,8 +1393,14 @@ in {
                   then "config"
                   else null
                 } = removeAttrs task.nomad.config [
-                  # must be removed to build a new image for the wrapper through the default value
+                  # must be removed to newly build for the wrapper through the default value
+
+                  # podman driver
                   "image"
+
+                  # exec driver
+                  "store_paths"
+                  "command"
                 ];
 
                 # propagate max dependencies' resources up to wrapper job

commit 78f1dc354b94c6b71abcffb8b8a2d4aef4268f19

fix creation of working dir in nomad job
broken in 1cb7cffbba1ce1a7146d1e310b167f2309db08d5

nix/module.nix
@@ -1031,6 +1031,12 @@
                 runtimeInputs = task.dependencies;
                 text = ''
                   ${__concatStringsSep "\n" (lib.mapAttrsToList (k: v: "export ${k}=${lib.escapeShellArg v}") config.env)}
+
+                  if [[ -n "$NOMAD_JOB_ID" ]]; then
+                    mkdir -p ${lib.escapeShellArg task.workingDir}
+                    cd ${lib.escapeShellArg task.workingDir}
+                  fi
+
                   #shellcheck disable=SC2288
                   exec ${task.computedCommand}/bin/${lib.escapeShellArg task.name}
                 '';
@@ -1344,13 +1350,6 @@ in {
             dependencies = [pkgs.tullia];
 
             command.text = ''
-              set +x
-              if [[ -n "$NOMAD_JOB_ID" ]]; then
-                mkdir /repo
-                cd /repo
-              fi
-              set -x
-
               exec tullia run ${lib.escapeShellArg name}
             '';
 